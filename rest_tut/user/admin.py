ca75ea2e5f227997a5644069ec5af0ee83137c25
NarekHayrapetyan <narek.hayrapetyan.68@gmail.com>
2024-05-25 15:14:11 +0400

    Added costum user model

--- rest_tut/user/admin.py
+++ rest_tut/user/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import User
 
 admin.site.site_header = "Blog Administration"
 admin.site.site_title = "Blog Admin Portal"
 admin.site.index_title = "Welcome to Blog Admin"
 
-admin.site.register(Post)
+admin.site.register(User)
